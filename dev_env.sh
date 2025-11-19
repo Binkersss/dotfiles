9cd6e7165011e671716226204d1a06c48077a581
Nathaniel Chappelle <nathaniel.chappelle@proton.me>
2025-11-18 22:01:57 -0800

    Added aur, ghostty, hypr; ghostty exec in hypr for vm

--- dev_env.sh
+++ dev_env.sh
@@ -2,6 +2,21 @@
 git checkout main
 git pull 
 
+echo "Updating system..."
+#sudo pacman -Syu --noconfirm
+
+echo "Installing/Updating base packages"
+if [[ -f "./base.packages" ]]; then
+	packages=$(grep -v '^#' "./base.packages" | grep -v '^$' | tr '\n' ' ')
+	if [[ -n "$packages" ]]; then
+		echo "$(sudo pacman -S --needed --noconfirm $packages)"
+	fi
+else
+	echo "Warning: base.packages not found"
+fi
+
+echo "Make sure to install the packages in aur.packages separately!"
+
 make_symlink() {
     local target="$1"
     local linkpath="$2"
@@ -17,3 +32,13 @@ make_symlink() {
 make_symlink "${PWD}/config/nvim" "${XDG_CONFIG_HOME:-$HOME/.config}/nvim"
 
 make_symlink "${PWD}/config/tmux" "${XDG_CONFIG_HOME:-$HOME/.config}/tmux"
+
+make_symlink "${PWD}/config/ghostty" "${XDG_CONFIG_HOME:-$HOME/.config}/ghostty"
+
+make_symlink "${PWD}/config/hypr" "${XDG_CONFIG_HOME:-$HOME/.config}/hypr"
+
+make_symlink "${PWD}/config/wofi" "${XDG_CONFIG_HOME:-$HOME/.config}/wofi"
+
+make_symlink "${PWD}/config/waybar" "${XDG_CONFIG_HOME:-$HOME/.config}/waybar"
+
+make_symlink "${PWD}/config/dolphin/dolphinrc" "${XDG_CONFIG_HOME:-$HOME/.config}/dolphinrc"
